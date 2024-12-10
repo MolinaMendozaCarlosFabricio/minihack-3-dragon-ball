5398d6ea9b4919c074db9aafd97c6671da11e45c
MolinaMendozaCarlosFabricio <233376@ids.upchiapas.edu.mx>
2024-12-10 07:50:19 -0600

    Vista de episodios favoritos

--- src/app/home/view-home/view-home.component.ts
+++ src/app/home/view-home/view-home.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FavouriteEpisodesService } from '../../personajes/service/favourite-episodes.service';
 
 @Component({
   selector: 'app-view-home',
   templateUrl: './view-home.component.html',
   styleUrl: './view-home.component.css'
 })
-export class ViewHomeComponent {
+export class ViewHomeComponent implements OnInit {
   open_all_episodes: boolean = true;
   open_favourite_episodes: boolean = false;
+  id_favourite_episodes: number[] = [];
+
+  constructor(private favouriteEpisodesServices: FavouriteEpisodesService){}
+
+  ngOnInit(): void {
+      this.get_favourite_episodes();
+  }
+
+  get_favourite_episodes(){
+    this.id_favourite_episodes = this.favouriteEpisodesServices.get();
+  }
 
   open_ae(){
     this.open_all_episodes = true;
